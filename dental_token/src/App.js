27d00c455717309d0a8c4c07f548230729cdce22
Tashi Pema <12210035.gcit@rub.edu.bt>
2024-05-11 20:28:27 +0000

    added doctor

--- dental_token/src/App.js
+++ dental_token/src/App.js
@@ -1,14 +1,28 @@
-// import logo from './logo.svg';
-// import './App.css';
 import React from "react";
 import MainRouter from "./MainRouter";
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Active from './doctor/ActiveAppointment/Active/ActiveApp';
+import Maindash from './doctor/components/MainDash/Maindah';
+import Login from './doctor/components/Login';
+
 
 function App() {
   return (
     <>
-    <MainRouter/>
+      <MainRouter />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/dashboard' element={<Maindash />} />
+          <Route path='/activeapp' element={<Active />} />
+          <Route path='*' element={<h1>Not Found</h1>} />
+        </Routes>
+      </Router>
     </>
-  );
+
+  )
 }
+
 
 export default App;
---
+++ dental_token/src/doctor/components/SideBar/Sidebar.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+// import './Sidebar.css'
+import "../SideBar/Sidebar.css";
+// import Logo from '../../image/fin LOGO.png';
+import Logo from "../../image/fin LOGO.png";
+import { Link } from "react-router-dom";
+// import './../../App.css';
+import "../../../App.css";
+import { TbReportAnalytics } from "react-icons/tb";
+import { TbFileText } from "react-icons/tb";
+import { FiLogOut } from "react-icons/fi";
+
+const Sidebar = () => {
+    return (
+        <div className="Sidebar">
+            <div className="logo">
+                <img src={Logo} alt="" />
+            </div>
+
+            <div className="line"></div>
+
+            <div className="menu">
+                    <div className="menuItem ">
+                        <span style={{fontSize:"20px"}}><TbReportAnalytics /></span>
+                        {/* <span>Appointment</span> */}
+                        <Link exact to="/dashboard">Appointment</Link>
+                    </div>
+                    <div className="menuItem">
+                        <span style={{fontSize:"20px"}}><TbFileText /></span>
+                        {/* <span>Active Appointment</span> */}
+                        <Link to="/activeapp">Active Appointment</Link>
+                    </div>
+
+            
+
+            </div>
+            <div className="line line2"></div>
+
+            <div className="doctorInfo">
+                <div className="div1">
+                    <p className="name">Tashi Wangyal</p>
+                    <p>Dentist</p>
+                </div>
+
+                <div className="info-btn">
+            <button style={{backgroundColor:"#373c3e", color:"white"}}><span style={{fontSize:"19px"}}><FiLogOut /> </span>Log Out</button>
+                </div >
+            
+
+            </div>
+
+
+
+
+        </div>
+    )
+}
+
+export default Sidebar
