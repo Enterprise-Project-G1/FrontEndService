14d4bb045f2cda72b6c4abc842a4a5e2b281b47f
Tashi Pema <12210035.gcit@rub.edu.bt>
2024-05-12 15:14:00 +0000

    add users

--- dental_token/src/Mrouters.jsx
+++ dental_token/src/Mrouters.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // import Home from './components/home'; // Assuming you have a Home component
-import Home from "./components/home";
-import Team from './components/team'; // Assuming you have a Team component
-import Token from './components/token'; // Assuming you have a Token component
-import Notification from './components/notification'; // Assuming you have a Notification component
-import Profile from './components/profile'; // Assuming you have a Profile component
-import Login from './components/login';
-import Signup from './components/signup';
+// import Home from "./components/home";
+import Home from "./user/components/home"
+// import Team from './components/team'; // Assuming you have a Team component
+import Team from "./user/components/team";
+// import Token from './components/token'; // Assuming you have a Token component
+import Token from "./user/components/token";
+// import Notification from './components/notification'; // Assuming you have a Notification component
+import Notification from "./user/components/notification";
+// import Profile from './components/profile'; // Assuming you have a Profile component
+import Profile from "./user/components/profile";
+// import Login from './components/login';
+import Login from './user/components/login';
+import Signup from './user/components/signup';
+// import Signup from './components/signup';
 
 function Mrouters() {
   return (
