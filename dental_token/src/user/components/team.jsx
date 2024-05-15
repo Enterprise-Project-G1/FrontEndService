7ffaf063b128cb3421599f5505d62a1a57d2ef03
Tashi Pema <12210035.gcit@rub.edu.bt>
2024-05-15 18:59:47 +0000

    edited team page

--- dental_token/src/user/components/team.jsx
+++ dental_token/src/user/components/team.jsx
@@ -1,20 +1,25 @@
+// Team.jsx
+
 import React from "react";
 import { Link } from "react-router-dom"; // Import Link from react-router-dom
 import Doctors from "../img/doctors.png";
 import Tashi from "../img/tashi.png";
+// import Header from "../"
+import Header from "../components/header";
+import Nav from "../components/navigation";
+import Footer from "../components/footer";
+// import Tenzin from "../img/tenzin.png";
+// import Sancho from "../img/sancho.png";
+import { Icon } from "@iconify/react";
+
 
 import "../css/team.css";
-import Header from "./header";
-import Footer from "./footer";
-import Nav from "./navigation";
 
 const Team = () => {
   return (
-    <>
-     <Header/>
-      <Nav/>
     <div className="team-container">
-     
+      <Header/>
+      <Nav/>
       <div className="team-section">
         <div className="team-info">
           <div className="team-text">
@@ -37,15 +42,62 @@ const Team = () => {
           </div>
         </div>
       </div>
+      
       {/* Next section with three boxes */}
       <div className="team-boxes">
+        <div className="section">
+          <h3 className='section-title'>Our Team</h3>
+          <div className="section-layer">
+            <hr className='hr1'></hr>
+            <Icon className='section-icon' icon="mdi:tooth-outline" />
+            {/* <div className='hr'></div> */}
+            <hr className='hr2'></hr>
+          </div>
+      </div>
         <div className="team-box">
           <div className="image-section">
             <img src={Tashi} alt="Team Member 1" />
           </div>
           <div className="text-section">
-            <h3>Tashi Pema</h3>
-            <p>Receptionist</p>
+            <h1>Tashi Pema</h1>
+            <h3>Receptionist</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>  
+          </div>
+        </div>
+
+        <div className="team-box">
+          <div className="image-section">
+            <img src={Tashi} alt="Team Member 1" />
+          </div>
+          <div className="text-section">
+            <h1>Tashi Pema</h1>
+            <h3>Receptionist</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
@@ -63,8 +115,8 @@ const Team = () => {
             <img src={Tashi} alt="Team Member 1" />
           </div>
           <div className="text-section">
-            <h3>Tashi Pema</h3>
-            <p>Receptionist</p>
+            <h1>Tashi Pema</h1>
+            <h3>Receptionist</h3>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
@@ -74,16 +126,6 @@ const Team = () => {
               nulla pariatur. Excepteur sint occaecat cupidatat non proident,
               sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
-          </div>
-        </div>
-
-        <div className="team-box">
-          <div className="image-section">
-            <img src={Tashi} alt="Team Member 1" />
-          </div>
-          <div className="text-section">
-            <h3>Tashi Pema</h3>
-            <p>Receptionist</p>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
@@ -96,10 +138,8 @@ const Team = () => {
           </div>
         </div>
       </div>
-      
+      <Footer/>
     </div>
-    <Footer/>
-    </>
   );
 };
 
